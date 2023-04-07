e1937c181fd5e2985282579519692eabeb735896
muhammed ozcan <muhammedozcan786@gmail.com>
2023-04-07 16:20:54 +0300

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        <h5>about</h5>
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <nuxt-link to="/about">about</nuxt-link>
+    <Tutorial />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
